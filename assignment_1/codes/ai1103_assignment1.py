1479cdeff2d0ce8f5f8dc99236c62eb5d08878b3
DineshAvulaMohanaDurga <80453560+DineshAvulaMohanaDurga@users.noreply.github.com>
2021-03-20 14:10:30 +0530

    Add files via upload

--- assignment_1/codes/ai1103_assignment1.py
+++ assignment_1/codes/ai1103_assignment1.py
@@ -1,18 +1,7 @@
 from scipy.stats import binom
 import numpy as np
-#simlen=number of stimulation trials
-simlen=5000000
-#probability of item to be defective
-p=5/100
 k=1
 n=10
-prob_theor=binom.cdf(k,n,p)
-data_binom=binom.rvs(n,p,size=simlen)
-#stimulating the event of selecting 10 items with less than 1 defective item
-err_ind=np.nonzero(data_binom<=k)
-err_n =np.size(err_ind)
-print("The following results are obtained by stimulation")
-print("The probability that the sample contains atmost one defective item is",err_n/simlen,"\n")
-print("The following value is obtained manually")
-print("The probability that the sample contains atmost one defective item is",prob_theor)
-print("as you can see the both values are close")
+p=5/100
+prob_stim = binom.cdf(k,n,p)
+print("The probability that the sample contains less than one defective is",prob_stim)
