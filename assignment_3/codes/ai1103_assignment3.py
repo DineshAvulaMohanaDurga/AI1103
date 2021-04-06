a44f0704d700531e050dbf69524404aa4ba38486
DineshAvulaMohanaDurga <80453560+DineshAvulaMohanaDurga@users.noreply.github.com>
2021-04-06 09:47:58 +0530

    Add files via upload

--- assignment_3/codes/ai1103_assignment3.py
+++ assignment_3/codes/ai1103_assignment3.py
@@ -1,11 +1,10 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import random as rand
-num_trial=500
+num_trial=800
 def fact(k):
     f=i=1
     while i<=k:
-        f*=i
+        f=f*i
         i+=1
     return f
 
@@ -19,22 +18,32 @@ def term(r,p,n):
     term=comb(n,r)*pow(p,r)*pow(1-p,n-r)
     return term
 
-probabilities=[1.0,0.1]
+probabilities=[]
 i=0
+lists=[]
 while i<num_trial:
-    probabilities[i]=rand.random()
-i=j=0
-values=[0.0,0.0]
+  temp=rand.uniform((i+num_trial)/(num_trial*2+1),(i+1+num_trial)/(num_trial*2+1))
+  probabilities.append(temp)
+  lists.append(i)
+  i+=1
+j=0
+i=0
+probabilities.sort()
+values=[]
 while i<num_trial:
-    j=0
+    j=temp=0
     while j<num_trial:
-        values[i]=values[i]+term(i,probabilities[j],num_trial)
+        temp=temp+term(i,probabilities[j],num_trial)
+        j+=1
+    i+=1
+    values.append(temp)
+
 #plotting graph
-probability=np.arange(probabilities)
-plt.plot(probability,values)
+fig=plt.figure(figsize=(10,5))
+plt.plot(lists,values)
 plt.title('values vs Y')
 plt.xlabel('Y')
 plt.ylabel('value')
 
-plt.savefig("figure1.png")
+fig.savefig("figure1.png")
 plt.show()
